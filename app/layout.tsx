9eb222cc50f423b9e64b08c9102999954973e1d7
wisdom-technologies-bit <wisdomtechnologies387@gmail.com>
2026-06-19 02:08:09 +0100

    Refactor script tag to use dangerouslySetInnerHTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,9 +40,11 @@ export default function RootLayout({
     async
     data-cfasync="false"
   ></script>
-    <Script id="monetag-vignette">
-{`(function(s){s.dataset.zone='11169801',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
-</Script>
+    <script
+  dangerouslySetInnerHTML={{
+    __html: `(function(s){s.dataset.zone='11169801',s.src='https://n6wxm.com/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
+  }}
+/>
 </head>
 
   <body className={`${inter.className} bg-background text-foreground`}>
