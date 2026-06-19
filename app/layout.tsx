2a651a722d534065be673e3344d6284a7212e0e4
wisdom-technologies-bit <wisdomtechnologies387@gmail.com>
2026-06-19 01:52:59 +0100

    Add monetag-vignette script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,17 @@ export default function RootLayout({
     async
     data-cfasync="false"
   ></script>
+    <Script id="monetag-vignette">
+  {`
+    (function(s){
+      s.dataset.zone='11169801',
+      s.src='https://n6wxm.com/vignette.min.js'
+    })([document.documentElement, document.body]
+      .filter(Boolean)
+      .pop()
+      .appendChild(document.createElement('script')))
+  `}
+</Script>
 </head>
 
   <body className={`${inter.className} bg-background text-foreground`}>
